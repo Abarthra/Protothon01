24fa4e068229e2f6fce1fe4148ff79b9788d5450
Abarthra <47408923+Abarthra@users.noreply.github.com>
2019-03-16 01:35:26 -0700

    Add files via upload

---
+++ scnd prgrm.c
@@ -0,0 +1,21 @@
+import math
+
+number=input("enter the comma seperated numbers")
+
+sum=0
+
+x=number.split(",")
+
+y=len(x)
+
+for i in x:
+
+    i=float(i)
+
+    sum=sum+(i*i)
+
+average=(sum)/y
+
+rms=math.sqrt(average)
+
+print(rms)
